8741e6477768faf3a18aacebe6b3c97438627577
Muto Sota <hasu_m.s@icloud.com>
2023-07-11 23:06:41 +0900

    Initial Commit

---
+++ UDP/UDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  UDPApp.swift
+//  UDP
+//
+//  Created by User on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct UDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
