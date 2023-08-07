b930081726c1b6951e37ead22c926a9a21533b1f
Muto Sota <hasu_m.s@icloud.com>
2023-08-07 15:50:24 +0900

    8/7

---
+++ UDP/UDPServerBridge.h
@@ -0,0 +1,20 @@
+//
+//  UDPServerBridge.h
+//  UDP
+//
+//  Created by User on 2023/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UDPServerBridge : NSObject
+
+typedef void (^MessageHandler)(NSString *);
+
+- (void)startUDPServerWithHandler:(MessageHandler)handler;
+
+@end
+
+NS_ASSUME_NONNULL_END
